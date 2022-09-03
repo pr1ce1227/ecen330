9f34a316f11168007d2b1113cbd8f2655722ea58
Caleb Price <calebsp12@gmail.com>
2022-09-03 11:32:42 -0600

    Lab 1 Complete

--- lab1_helloworld/main.c
+++ lab1_helloworld/main.c
@@ -13,9 +13,18 @@ For questions, contact Brad Hutchings or Jeff Goeders, https://ece.byu.edu/
 
 #include "display.h"
 
-#define TEXT_SIZE 2
-#define CURSOR_X 10
-#define CURSOR_Y (DISPLAY_HEIGHT / 2)
+#define halfHeight DISPLAY_HEIGHT / 2
+#define halfWidth DISPLAY_WIDTH / 2
+#define origin 0
+#define radius 25
+#define quarterWidth DISPLAY_WIDTH * .25
+#define threeQuarterWidth DISPLAY_WIDTH * .75
+#define thirdHeight DISPLAY_HEIGHT * .33
+#define twoThirdHeight DISPLAY_HEIGHT * .66
+#define thirdWidth DISPLAY_WIDTH * .33
+#define twoThirdWidth DISPLAY_WIDTH * .66
+#define tenthHeight DISPLAY_HEIGHT * .1
+#define nineTenthHeight DISPLAY_HEIGHT * .9
 
 // Print out "hello world" on both the console and the LCD screen.
 int main() {
@@ -24,15 +33,20 @@ int main() {
   display_init();
   display_fillScreen(DISPLAY_BLACK); // Blank the screen.
 
-  // Configure display text settings
-  display_setTextColor(DISPLAY_RED); // Make the text red.
-  display_setTextSize(TEXT_SIZE);    // Make the text a little larger.
+  // 2 green crossing lines
+  display_drawLine(origin, origin, DISPLAY_WIDTH, DISPLAY_HEIGHT,
+                   DISPLAY_GREEN);
+  display_drawLine(origin, DISPLAY_HEIGHT, DISPLAY_WIDTH, origin,
+                   DISPLAY_GREEN);
 
-  // Set the cursor location and print to the LCD
-  display_setCursor(CURSOR_X, CURSOR_Y);
-  display_println("hello world (on the LCD)!");
+  // 2 circles, one empty one filled
+  display_drawCircle(quarterWidth, halfHeight, radius, DISPLAY_RED);
+  display_fillCircle(threeQuarterWidth, halfHeight, radius, DISPLAY_RED);
 
-  // Also print out 'hello world' on the terminal (stdout).
-  printf("hello world!\n");
+  // 2 triangles, one empty one filled
+  display_fillTriangle(halfWidth, thirdHeight, thirdWidth, tenthHeight,
+                       twoThirdWidth, tenthHeight, DISPLAY_WHITE);
+  display_drawTriangle(halfWidth, twoThirdHeight, thirdWidth, nineTenthHeight,
+                       twoThirdWidth, nineTenthHeight, DISPLAY_YELLOW);
   return 0;
 }
